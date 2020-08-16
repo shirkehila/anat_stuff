1cfce6fa3e149835be3cae9b95be1f1affb45a90
Shir Kehila <shirkpro@gmail.com>
2020-08-16 23:10:57 +0300

    Will refactor browseWeb and add a config file for it

--- browseWeb.py
+++ browseWeb.py
@@ -1,23 +1,62 @@
 import time
 import datetime
-
-url = r'https://python.org'
+import json
+import os
 from selenium import webdriver
 
-filename = r"C:\pe\browseDuration.txt"
+BASE_PATH = ''
+OS = ''
+OUTPUT_FILENAME = ''
+CHROME_DRIVER_PATH = ''
+OUTPUT_FILE_PATH = ''
 
-options = webdriver.ChromeOptions()
-options.add_argument("--no-sandbox")
 
-d = webdriver.Chrome(r"c:\pe\chromedriver.exe", options=options)
-startTs = datetime.datetime.now()
-d.get('https://python.org')
+def get_config():
+    global BASE_PATH
+    global OS
+    global OUTPUT_FILENAME
+    with open('config.json', 'rt') as f:
+        json_config = json.load(f)
+        OS = json_config['os']
+        BASE_PATH = json_config['base_path'][OS]
+        OUTPUT_FILENAME = json_config['output_filename']
 
-while not (d.title == "Welcome to Python.org"):
-    time.sleep(0.1)
-endTs = datetime.datetime.now()
-duration = endTs - startTs
-print(duration)
-with open(filename, 'w') as f:
-    print(duration, file=f)
-d.quit()
+
+def create_paths():
+    global CHROME_DRIVER_PATH
+    global OUTPUT_FILE_PATH
+    CHROME_DRIVER_PATH = os.path.join(BASE_PATH, 'chromedriver.exe')
+    OUTPUT_FILE_PATH = os.path.join(BASE_PATH, OUTPUT_FILENAME)
+
+
+def init_webdriver():
+    options = webdriver.ChromeOptions()
+    options.add_argument("--no-sandbox")
+    d = webdriver.Chrome(CHROME_DRIVER_PATH, options=options)
+    return d
+
+
+def get_chrome_opening_duration():
+    start_timestamp = datetime.datetime.now()
+    d = init_webdriver()
+    d.get('https://python.org')
+
+    while not (d.title == "Welcome to Python.org"):
+        time.sleep(0.1)
+    end_timestamp = datetime.datetime.now()
+    d.quit()
+    duration = end_timestamp - start_timestamp
+    return duration
+
+
+def log_duration(duration):
+    print(duration)
+    with open(OUTPUT_FILE_PATH, 'w') as f:
+        print(duration, file=f)
+
+
+if __name__ == '__main__':
+    get_config()
+    create_paths()
+    duration = get_chrome_opening_duration()
+    log_duration(duration)
